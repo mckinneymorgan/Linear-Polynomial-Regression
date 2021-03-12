c30d058c144bef7588f8b141c6629200a9db511f
mckinneymorgan <43484031+mckinneymorgan@users.noreply.github.com>
2021-03-11 23:33:09 -0500

    Syntax, importation

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # Original author: Morgan McKinney 3/2021
 
+import csv
+
 # User input, get filename(s)
 print("LINEAR AND POLYNOMIAL REGRESSION \n")
 file = input("Enter csv file name: ")
-classLabelFileExist = False
-classLabelFile = input("Are the class labels in another file (Y/N): ")
-if classLabelFile.lower() == 'y':
-    classLabelFile = input("Enter class label file name: ")
-    classLabelFileExist = True
+classValueFileExist = False
+classValueFile = input("Are the class labels in another file (Y/N): ")
+if classValueFile.lower() == 'y':
+    classValueFile = input("Enter class label file name: ")
+    classValueFileExist = True
 featureNames = False
 featureNamesExist = input("Are the features named (Y/N): ")
 if featureNamesExist.lower() == 'y':
@@ -16,14 +18,14 @@ if featureNamesExist.lower() == 'y':
 # Read file
 names = []
 data = []
-classLabelName = 0
-classLabels = []
+classValueName = 0
+classValues = []
 with open(file) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
 
     # Count features, return to start of file
     featureCount = len(next(readCSV))
-    if not classLabelFileExist:
+    if not classValueFileExist:
         featureCount -= 1
     csvfile.seek(0)
     print("\nFeature count: ")
@@ -32,7 +34,7 @@ with open(file) as csvfile:
     # Name features, if provided
     if featureNames:
         names = next(readCSV)
-        if not classLabelFileExist:
+        if not classValueFileExist:
             names.pop()
     else:
         for x in range(featureCount):
@@ -49,30 +51,30 @@ with open(file) as csvfile:
         data.append(rowList)
 
     # Populate class list
-    if not classLabelFileExist:
+    if not classValueFileExist:
         csvfile.seek(0)
         for row in readCSV:
-            classLabels.append(row[featureCount])
+            classValues.append(row[featureCount])
 
 # Open class label file if applicable
-if classLabelFileExist:
-    with open(classLabelFile) as csvfile:
+if classValueFileExist:
+    with open(classValueFile) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
 
         # Populate class list
         if featureNamesExist:
-            classLabelName = next(readCSV)
+            classValueName = next(readCSV)
         for row in readCSV:
-            classLabels.append(row[0])
+            classValues.append(row[0])
 
 print("Class label name: ")
-print(classLabelName)
+print(classValueName)
 
 # Append class labels to data set
 entry = -1
 for x in data:
     entry += 1
-    classLabel = classLabels[entry]
+    classLabel = classValues[entry]
     if classLabel == 'True':
         classLabel = 1
     elif classLabel == 'False':
