78f1d29a79cb559c3ce7658d9f10fcd9be116b6b
mckinneymorgan <43484031+mckinneymorgan@users.noreply.github.com>
2021-03-15 23:59:20 -0400

    Definitions of regression.py

--- regression.py
+++ regression.py
@@ -3,9 +3,14 @@
 import math
 import numpy
 
+def preprocess():
 
 def get_h_theta(m, x):
     # Reference: https://www.geeksforgeeks.org/transpose-matrix-single-line-python/
     transposed_theta = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
     h_theta = transposed_theta * x
     return h_theta
+
+def weight_update():
+    # Calculate h_theta(x) = theta^T * x
+    # Calculate raw error
