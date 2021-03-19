00c14b6c6100e402f358002b2474ef2e497bd7e8
mckinneymorgan <43484031+mckinneymorgan@users.noreply.github.com>
2021-03-18 23:59:08 -0400

    Graphing data start

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import read
 import regression
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Initialize variables
 names = []
@@ -12,6 +14,7 @@ classValues = []
 # User input, read and store input csv file
 print("LINEAR AND POLYNOMIAL REGRESSION \n")
 read.read_file(names, classValueName, data, classValues)
+data = np.array(data)  # Store data column-wise
 featureCount = len(data[0])-1
 newFeatureValues = [[y for y in x] for x in data]
 
@@ -21,6 +24,15 @@ print("Features:")
 print(featureCount)
 print("New feature values:")
 print(newFeatureValues)
+
+# Plot data
+x = []
+y = []
+plt.title("Linear Regression")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.plot(x, y, color ="green")
+plt.show()
 
 
 
