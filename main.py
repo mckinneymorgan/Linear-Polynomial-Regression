5391cbdd705be2485714b72b042863ce0bc41f6f
mckinneymorgan <43484031+mckinneymorgan@users.noreply.github.com>
2021-03-22 01:19:31 -0400

    Polynomial graphing functional

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ epochMax = 100  # Full batch
 theta_0 = randint(0, 1)
 theta_1 = randint(0, 1)
 linear = True
-order = 11  # Number of features in synthetic dataset
+order = 11  # Number of features in synthetic dataset, default
 
 # User input, read and store input csv file
 print("LINEAR AND POLYNOMIAL REGRESSION \n")
 read.read_file(names, data)
 data = np.array(data)  # Store data column-wise
-regressionType = input("Linear or polynomial regression?")
-if regressionType.lower() == 'polynomial':
+regressionType = input("Linear or polynomial regression? ")
+if regressionType.lower() == 'polynomial' or regressionType.lower() == 'poly':
     linear = False
-    orderStr = input("What is the order?")
+    orderStr = input("What is the order? ")
     order = int(orderStr)
 elif regressionType.lower() != 'linear':
     sys.exit("Invalid input")
@@ -42,13 +42,14 @@ print("New feature values:")
 print(newFeatureValues)
 
 # Plot data
-x = []
-y = []
-plt.title("Linear Regression")
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
-plt.plot(x, y, color="green")
-plt.show()
+if not linear:  # Only works for polynomial regression
+    x = [i[0] for i in data]  # First column
+    y = [i[featureCount] for i in data]  # Prediction column
+    plt.title("Linear Regression")
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.scatter(x, y, color="green")
+    plt.show()
 
 
 
